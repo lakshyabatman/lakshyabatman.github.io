e2304ffca6e3d4fbc2e7898f26cf2ef4f191bf3f
Lakshya Khera <lakshya.khera@gmail.com>
2020-11-22 20:54:08 +0530

    Updates

---
+++ precache-manifest.a4ff3d4113ba9a403e35bd107e5ad7b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca14bb87335a96f2acc5c8da7ae21dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b44a0123222a823855a6",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "48f41e1406242093236e",
+    "url": "/static/css/main.4be3a997.chunk.css"
+  },
+  {
+    "revision": "b44a0123222a823855a6",
+    "url": "/static/js/2.316c9c1a.chunk.js"
+  },
+  {
+    "revision": "9f2d04d7dba436641481af936367a375",
+    "url": "/static/js/2.316c9c1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48f41e1406242093236e",
+    "url": "/static/js/main.5142c148.chunk.js"
+  },
+  {
+    "revision": "b9cc265e08a6f1432730",
+    "url": "/static/js/runtime-main.9ab50df7.js"
+  },
+  {
+    "revision": "7d45cb2c5b55c3424bcd6b8320470ef5",
+    "url": "/static/media/me.7d45cb2c.png"
+  }
+]);
