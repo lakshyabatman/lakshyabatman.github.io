3b4c063cd58a5c442944dce7cabc664ab026ddec
lakshyabatman <lakshya.khera@gmail.comt>
2022-01-07 19:53:02 +0530

    update

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,5 +1,9 @@
 export const PROJECTS = [
     {
+        title:"Notion Diary",
+        content:"A Simple application which bootstrap your notion pages into blogsite"
+    },
+    {
         title: '#100daysOfCode Twitter Bot',
         content: ' Simple Twitter bot which tweets behalf of the  user whenever user forgets to tweet.'
     },
@@ -7,21 +11,17 @@ export const PROJECTS = [
     title: 'Comment Box EsLint Plugin',
     content: "Simple EsLint plugin which adds a block comment before funtionn's declaration to enforce better code practices."
     },
-  {
-      title:'React Redux CLI',
-      content:'A light weight CLI which installs React application and setup Redux on single prompt.'
-  },
-  {
-      title:'Ticket booking service',
-      content:'A simple NodeJS API with features like book tickets,add movies, get schedules,automated ticket expiration, the application architecture follows repository design pattern.'
-  },
-  {
-      title:'P2P video chat platform',
-      content:'Simple Peer 2 peer video chat platform using webrtc and signalhub. The application is bundled using webpack and served on NodeJS server.'
-  },
-  {
-      title:"Automated Birthday Wisher",
-      content:"Developed a Python Script which lets user to login via their google account and this script sends birthday wishes to their google contact using calendar API. Tech Stack: Python"
-  }, 
+    {
+        title:'React Redux CLI',
+        content:'A light weight CLI which installs React application and setup Redux on single prompt.'
+    },
+    {
+        title:'Ticket booking service',
+        content:'A simple NodeJS API with features like book tickets,add movies, get schedules,automated ticket expiration, the application architecture follows repository design pattern.'
+    },
+    {
+        title:"Automated Birthday Wisher",
+        content:"Developed a Python Script which lets user to login via their google account and this script sends birthday wishes to their google contact using calendar API. Tech Stack: Python"
+    }, 
   
 ]
